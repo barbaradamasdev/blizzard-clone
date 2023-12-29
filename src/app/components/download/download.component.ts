38e4ed7a90bd5b3bc93fd9f2176887c222c78c0d
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-12-29 13:17:58 -0300

    main component add

---
+++ src/app/components/download/download.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download',
+  standalone: true,
+  imports: [],
+  templateUrl: './download.component.html',
+  styleUrl: './download.component.css'
+})
+export class DownloadComponent {
+
+}
