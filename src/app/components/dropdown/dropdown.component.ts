aadbf1991541f25fb18e4909abdcffb5244ffc5e
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-01-02 12:04:41 -0300

    initiate dropdown code

---
+++ src/app/components/dropdown/dropdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './dropdown.component.html',
+  styleUrl: './dropdown.component.css'
+})
+export class DropdownComponent {
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DropdownComponent } from "../dropdown/dropdown.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+    imports: [DropdownComponent]
 })
 export class HeaderComponent {
 
