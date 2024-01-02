d7938c35760640b5831721a2fe51d7669da71d50
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-01-02 11:59:57 -0300

    update 5 games banner

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -15,11 +15,13 @@ export class BannerComponent {
     { id: 1, title: 'Retorna à escuridão com o game Diablo IV', name: 'Diablo IV', icon: 'game-1.png', description: 'O retorno de Lilith traz uma era de escuridão e sofrimento' , button: 'Jogue agora', src: 'diablo-animation-cover.png', trailer: 'diablo-animation.gif', bigIcon: 'diablo-logo.png', bgImage: 'diablo-bg.png'},
     { id: 2, title: 'Novo pacote de expansão de Hearthstone', name: 'Hearthstone', icon: 'game-2.png', description: 'A Horda e a Aliança se encontraram no Vale Alterac para lutar' , button: 'Reserve agora na pré-venda', src: 'hearthstone-animation-cover.png', trailer: 'hearthstone-animation.gif', bigIcon: 'hearthstone-logo.png', bgImage: 'hearthstone-bg.png'},
     { id: 3, title: 'Desbrave as Terras Sombrias em Shadowlands!', name: 'Warcraft', icon: 'game-3.png', description: 'O que jaz além do mundo que você conhece?' , button: 'Reserve agora na pré-venda', src: 'wow-animation-cover.png', trailer: 'wow-animation.gif', bigIcon: 'wow-logo.png', bgImage: 'wow-bg.png'},
-    { id: 4, title: 'Retorna à escuridão com o game Diablo IV', name: 'Diablo', icon: 'game-4.png', description: 'O retorno de Lilith traz uma era de escuridão e sofrimento' , button: 'Jogue agora', src: 'diablo-animation-cover.png', trailer: 'diablo-animation.gif', bigIcon: 'diablo-logo.png', bgImage: 'diablo-bg.png'},
-    { id: 5, title: 'Retorna à escuridão com o game Diablo IV', name: 'StarCraft', icon: 'game-5.png', description: 'O retorno de Lilith traz uma era de escuridão e sofrimento' , button: 'Jogue agora', src: 'diablo-animation-cover.png', trailer: 'diablo-animation.gif', bigIcon: 'diablo-logo.png', bgImage: 'diablo-bg.png'},
+    { id: 4, title: 'Um capítulo inédito na saga de diablo', name: 'Diablo', icon: 'game-4.png', description: 'Enfrente males ancestrais e descubra o que aconteceu' , button: 'Jogue agora', src: 'diablo-immortal-animation-cover.png', trailer: 'diablo-immortal-animation.gif', bigIcon: 'diablo-immortal-logo.png', bgImage: 'diablo-immortal-bg.png'},
+
+    { id: 5, title: 'O supremo jogo de estratégia em tempo real', name: 'StarCraft', icon: 'game-5.png', description: 'Jogue do seu jeito' , button: 'Jogue agora', src: 'starcraft-2-animation-cover.png', trailer: 'starcraft-2-animation.gif', bigIcon: 'starcraft-2-logo.png', bgImage: 'starcraft-2-bg.png'},
   ];
 
   @Output() gameChanged = new EventEmitter<Game>();
+  @Output() gameIdChanged = new EventEmitter<number>();
 
   currentGame: Game = this.games[0];
   isTrailerPlaying: boolean = false;
@@ -27,6 +29,7 @@ export class BannerComponent {
   selectGame(game: Game): void {
     this.currentGame = game;
     this.gameChanged.emit(game);
+    this.gameIdChanged.emit(game.id);
   }
 
   startTrailer() {
--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -13,6 +13,37 @@ import { Game } from '../../models/Game.model';
 export class HeroComponent {
   currentGame?: Game;
   backgroundStyle?: string;
+  selectedGameId?: number;
+
+  onGameIdChanged(id: number): void {
+    this.selectedGameId = id;
+    console.log(id)
+    console.log(this.selectedGameId)
+
+
+    switch (id) {
+      case 1:
+        this.setAfterWidth(20);
+        break;
+      case 2:
+        this.setAfterWidth(40);
+        break;
+      case 3:
+        this.setAfterWidth(60);
+        break;
+      case 4:
+        this.setAfterWidth(80);
+        break;
+      case 5:
+        this.setAfterWidth(100);
+        break;
+    }
+  }
+
+  private setAfterWidth(percentage: number): void {
+    const afterWidth = `${percentage}%`;
+    document.documentElement.style.setProperty('--after-width', afterWidth);
+  }
 
   updateBackground(game: Game): void {
     this.currentGame = game;
