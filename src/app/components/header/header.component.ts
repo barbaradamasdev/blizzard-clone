17d69cdba2dfcf2ce2023357589a327700055331
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-01-02 14:13:38 -0300

    update header background color

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { DropdownComponent } from "../dropdown/dropdown.component";
 
 @Component({
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
-    imports: [DropdownComponent, CommonModule]
+    imports: [CommonModule]
 })
 export class HeaderComponent {
   isDropdownVisible = false;
   menuIconSrc = '../../../assets/menu-burguer.svg';
 
-
   toggleDropdown(): void {
     this.isDropdownVisible = !this.isDropdownVisible;
     this.menuIconSrc = this.isDropdownVisible ? '../../../assets/menu-open.svg' : '../../../assets/menu-burguer.svg';
-
   }
 }
