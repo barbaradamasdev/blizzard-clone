b6c24f9d9c9f61ad78d73b5f5660af177b2e61ff
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-01-03 19:49:36 -0300

    create function to update OS on footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  iconOs = '../../../assets/menu-burguer.svg';
+  title?: string;
+
+  ngOnInit() {
+    const userAgent = window.navigator.userAgent;
+
+    if (userAgent.includes('Windows')) {
+      this.updateOS('Windows');
+    } else if (userAgent.includes('MacOS')) {
+      this.updateOS('MacOS');
+    } else if (userAgent.includes('Linux')) {
+      this.updateOS('Linux');
+    }
+  }
+
+  updateOS(sistemaOperacional:string): void {
+    this.iconOs = `../../../assets/os/${sistemaOperacional}.svg`;
+    console.log(this.iconOs)
+    this.title = sistemaOperacional;
+  }
 
 }
